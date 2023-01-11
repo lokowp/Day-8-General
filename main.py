603f1daf493badfde7c6ba5b708c746deccbc62b
Giorgos Georgiou <93381413+lokowp@users.noreply.github.com>
2023-01-11 09:39:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+
+# def greet():
+#   print("Hello world.")
+#   print("Welcome to the coding room.")
+#   print("We're still new so excuse us.")
+
+
+# greet()
+
+# function that allows for input
+# def greet_with_name(name):
+#   print(f"Hello {name}")
+#   print(f"How do you do {name}?")
+# greet_with_name("Angela")
+
+# Functions with more than 1 input
+def greet_with(name,location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+
+greet_with(location="Limassol", name="Loko")
